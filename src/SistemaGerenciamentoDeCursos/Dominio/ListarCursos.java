a0518b85f82ffb034fcbd9dd5679c4132d0dc07c
machado001 <machadowg@gmail.com>
2024-08-21 20:36:36 -0300

    bugfix: opção 4 não mostra nada no início do programa
    no inicio a lista de cursos esta vazia, e o usuario nao recebia resposta

--- src/SistemaGerenciamentoDeCursos/Dominio/ListarCursos.java
+++ src/SistemaGerenciamentoDeCursos/Dominio/ListarCursos.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 public class ListarCursos {
     public static void listar (ArrayList<Curso> cursos){
-        for (Curso c : cursos) {
-            System.out.println("Curso: " + c.getNome() + ", código do curso: " + c.getCodigoCurso());
-            c.getAlunos().forEach(a -> System.out.println("Aluno: " + a.getNome() + ", Matrícula: " + a.getNumeroMatricula()));
-            System.out.println("---------------------------------");
+        if (cursos == null || cursos.isEmpty()) {
+            System.out.println("Nenhum curso registrado ainda");
+        } else {
+            for (Curso c : cursos) {
+                System.out.println("Curso: " + c.getNome() + ", código do curso: " + c.getCodigoCurso());
+                c.getAlunos().forEach(a -> System.out.println("Aluno: " + a.getNome() + ", Matrícula: " + a.getNumeroMatricula()));
+            }
         }
+        System.out.println("---------------------------------");
     }
 }
